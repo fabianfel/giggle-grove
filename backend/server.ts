4957353ac6099a53ed35c3cae8a3ad99e95053ea
Fabian Feldherr <fabianf1@gmx.net>
2024-03-26 23:42:45 +0100

    changed from db to websocket connection

--- backend/server.ts
+++ backend/server.ts
@@ -3,197 +3,66 @@ import * as FastifyWebSocket from "@fastify/websocket";
 import Fastify, { FastifyInstance } from "fastify";
 
 import { SocketStream } from "@fastify/websocket";
-
-import { ConstantBackoff, handleAll, retry } from "cockatiel";
 import { config as loadEnv } from "dotenv";
-import { Etcd3, GRPCUnavailableError } from "etcd3";
-
-class Database {
-  private usedClient: Etcd3 | Map<string, string>;
-  private dbClient: Etcd3;
-
-  public constructor(options: { hostList: string[] }) {
-    console.log("Trying to connect to etcd");
-    this.dbClient = new Etcd3({
-      hosts: options.hostList,
-      faultHandling: {
-        global: retry(handleAll, {
-          backoff: new ConstantBackoff(1000),
-          maxAttempts: 3,
-        }),
-      },
-    });
-    this.usedClient = this.dbClient;
-  }
-
-  private reconnectTimer: NodeJS.Timeout;
-
-  private initMap(): Map<string, string> {
-    console.log("Error while trying to put value to etcd");
-    console.log("Trying to put value to Map");
-    this.reconnectTimer = setTimeout(async () => {
-      this.dbClient
-        .getAll()
-        .keys()
-        .then(() => (this.usedClient = this.dbClient))
-        .catch(() => {
-          console.log(
-            "Retry to connect to etcd failed. Trying again in 5 seconds."
-          );
-          this.reconnectTimer.refresh();
-        });
-    }, 5000);
-    return new Map();
-  }
-
-  public put(key: string) {
-    return {
-      value: (value: string) => {
-        return {
-          exec: () => {
-            if (this.usedClient instanceof Map) {
-              this.usedClient.set(key, value);
-              return Promise.resolve(true);
-            }
-
-            this.usedClient
-              .put(key)
-              .value(value)
-              .exec()
-              .then(
-                (value) => true,
-                (rej) => {
-                  if (rej instanceof GRPCUnavailableError) {
-                    this.usedClient = this.initMap();
-                    this.usedClient.set(key, value);
-                    return true;
-                  }
-                  console.log(rej);
-                  return false;
-                }
-              );
-          },
-        };
-      },
-    };
-  }
-
-  public get(key: string) {
-    return {
-      json: () => {
-        if (this.usedClient instanceof Map) {
-          const data = this.usedClient.get(key);
-          return data
-            ? Promise.resolve(JSON.parse(data))
-            : Promise.resolve(null);
-        }
-
-        return this.usedClient
-          .get(key)
-          .json()
-          .then(
-            (value) => (value ? value : null),
-            (rej) => {
-              if (rej instanceof GRPCUnavailableError) {
-                this.usedClient = this.initMap();
-                return this.usedClient.get(key);
-              }
-              console.log(rej);
-              return null;
-            }
-          );
-      },
-    };
-  }
-
-  public getAll() {
-    return {
-      keys: () => {
-        if (this.usedClient instanceof Map) {
-          return Promise.resolve(Array.from(this.usedClient.keys()));
-        }
-
-        return this.usedClient
-          .getAll()
-          .keys()
-          .then(
-            (keys) => keys,
-            (rej) => {
-              if (rej instanceof GRPCUnavailableError) {
-                this.usedClient = this.initMap();
-                return Promise.resolve(Array.from(this.usedClient.keys()));
-              }
-              console.log(rej);
-              return [];
-            }
-          );
-      },
-    };
-  }
-
-  public delete(
-    key?: string
-  ): Promise<boolean> | { all: () => Promise<boolean> } {
-    if (key != undefined && key != null && key != "") {
-      if (this.usedClient instanceof Map) {
-        return Promise.resolve(this.usedClient.delete(key));
-      }
-      return this.usedClient
-        .delete()
-        .key(key)
-        .then(
-          (value) => Number(value.deleted) > 0,
-          (rej) => {
-            if (rej instanceof GRPCUnavailableError) {
-              this.usedClient = this.initMap();
-              return this.usedClient.delete(key);
-            }
-            console.log(rej);
-            return false;
-          }
-        );
-    }
-    return {
-      all: () => {
-        if (this.usedClient instanceof Map) {
-          this.usedClient.clear();
-          return Promise.resolve(true);
-        }
-        return this.usedClient
-          .delete()
-          .all()
-          .exec()
-          .then(
-            (value) => Number(value.deleted) > 0,
-            (rej) => {
-              if (rej instanceof GRPCUnavailableError) {
-                this.usedClient = this.initMap();
-                return true;
-              }
-              console.log(rej);
-              return false;
-            }
-          );
-      },
-    };
-  }
-}
+import { v4 as uuidv4 } from "uuid";
+import { WebSocket } from "ws";
 
 interface Group {
-  messages: string[];
-  users: string[];
+  users: Set<string>;
+  conns: Set<SocketStream>;
 }
 
 interface Message {
-  operation: string;
-  payload?: { groupname: string; user: string; msg: string };
+  user: string;
+  msg: string;
+  ack: boolean;
+}
+
+interface ConnectionObject {
+  user: string;
+  groupname: string;
 }
 
 loadEnv();
 
-const dbClient = new Database({
-  hostList: process.env.DB_HOSTLIST!.split(","),
-});
+const serverID = uuidv4();
+
+const serverlist = new Set<WebSocket>();
+
+for (const host of process.env.DB_HOSTLIST!.split(",")) {
+  const currentTimer = setTimeout(() => {
+    const currSocket = new WebSocket(host + "/socket");
+
+    const reconnect = (_: any) => {
+      serverlist.delete(currSocket);
+      currentTimer.refresh();
+    };
+
+    currSocket.onclose = reconnect;
+    currSocket.onerror = reconnect;
+
+    currSocket.onopen = (_) => {
+      console.log("Connected to:", currSocket.url);
+      currSocket.send(
+        JSON.stringify({ operation: "REGISTER_SERVER", payload: { serverID } })
+      );
+      serverlist.add(currSocket);
+    };
+
+    currSocket.onmessage = (msg) => {
+      if (msg.data === "REQUESTED_SELF_REGISTER") {
+        console.log(
+          "Unregistering own server from serverlist:",
+          currSocket.url
+        );
+        clearInterval(currentTimer);
+        currSocket.onclose = null;
+        currSocket.close();
+        serverlist.delete(currSocket);
+      }
+    };
+  }, 1000);
+}
 
 const server: FastifyInstance = Fastify({ logger: true });
 server.register(FastifyWebSocket);
@@ -202,137 +71,154 @@ server.register(FastifyStatic, {
   root: process.cwd() + "/public",
 });
 
-const connections: Map<string, SocketStream> = new Map();
+const groups: Map<string, Group> = new Map();
 
-server.register(async function (server: FastifyInstance) {
+const conns: Map<SocketStream, ConnectionObject> = new Map();
+
+const disconnect = (conn: SocketStream) => {
+  const userInfo = conns.get(conn)!;
+  if (userInfo) {
+    const oldGroup = groups.get(userInfo.groupname);
+
+    oldGroup!.users.delete(userInfo.user);
+    oldGroup!.conns.delete(conn);
+    conns.delete(conn);
+
+    console.log(
+      "User:",
+      userInfo.user,
+      "in Group:",
+      userInfo.groupname,
+      "has been disconnected"
+    );
+  }
+};
+
+server.register(async (server: FastifyInstance) => {
   server.get("/", async (_request, _reply) => {
     return _reply.sendFile("./index.html");
   });
 
+  server.post("/socketLoop", async (request, _reply) => {
+    const toTry = process.env.DB_HOSTLIST!.split(",")[0];
+    const timer = setTimeout(() => {
+      new WebSocket(toTry + "/socket").onerror = () => {};
+      timer.refresh();
+    }, 0);
+  });
+
   server.get("/groups", async (_request, _reply) => {
-    return dbClient.getAll().keys();
+    return Array.from(groups.keys());
   });
 
   server.delete("/groups", async (_request, _reply) => {
-    return (dbClient.delete() as any).all();
+    return groups.clear();
   });
 
-  server.get("/socket", { websocket: true }, (connection: SocketStream) => {
+  server.get("/socket", { websocket: true }, (conn: SocketStream) => {
     // Client has been connected. Send active groups to client.
 
-    connection.socket.on("message", async (request) => {
-      const requestJson: Message = JSON.parse(request.toString());
+    conn.socket.on("message", async (request) => {
+      const requestJson = JSON.parse(request.toString());
 
-      const groupname = requestJson.payload!.groupname;
-      const user = requestJson.payload!.user;
+      const groupname = requestJson.payload?.groupname;
+      const user = requestJson.payload?.user;
 
-      let group: Group;
       let response = {};
+      let group = groups.get(groupname);
 
       switch (requestJson.operation) {
-        case "CREATE_OR_JOIN_GROUP":
-          group = (await dbClient.get(groupname).json()) as Group;
-
-          if (!group) {
-            group = {
-              messages: [],
-              users: [],
-            };
+        case "REGISTER_SERVER":
+          console.log("Registering new server");
+          if (requestJson.payload!["serverID"] !== serverID) {
+            conn.socket.send("REGISTERED");
+          } else {
+            conn.socket.send("REQUESTED_SELF_REGISTER");
           }
+          break;
 
-          if (group.users.includes(user)) {
+        case "CREATE_OR_JOIN_GROUP":
+          if (!group) {
+            console.log(
+              "Group:",
+              groupname,
+              "does not exist locally. Creating"
+            );
+            group = { users: new Set(), conns: new Set() };
+          } else if (group.users.has(user)) {
             console.log("User: " + user + " is already in group");
             response = {
               operation: "NAME_ALREADY_TAKEN_FOR_GROUP",
             };
-            connection.socket.send(JSON.stringify(response));
+            conn.socket.send(JSON.stringify(response));
             break;
+          } else if (conns.has(conn)) {
+            disconnect(conn);
           }
 
-          group.users.push(user);
+          group.users.add(user);
+          group.conns.add(conn);
 
-          const connectionKey = groupname + "_" + user;
-
-          connections.set(connectionKey, connection);
-
-          dbClient.put(groupname).value(JSON.stringify(group)).exec();
+          conns.set(conn, { groupname, user });
+          groups.set(groupname, group);
 
           response = {
             operation: "GROUP_JOINED",
           };
 
-          console.log("User " + user + " joined group ", group);
+          console.log("User", user, "joined group", groupname);
 
-          connection.socket.send(JSON.stringify(response));
+          conn.socket.send(JSON.stringify(response));
           break;
 
         case "SEND_MESSAGE":
-          group = (await dbClient.get(groupname).json()) as Group;
+          const newMsg = requestJson.payload!.msg;
+          console.log("Received message:", requestJson);
+
+          if (!requestJson.fromServer) {
+            for (const server of serverlist) {
+              server.send(
+                JSON.stringify({
+                  operation: "SEND_MESSAGE",
+                  fromServer: true,
+                  payload: { groupname, user, msg: newMsg },
+                })
+              );
+            }
+          }
 
           if (group) {
-            const newMsg = requestJson.payload!.msg;
-
             console.log(
-              "New Message received in Group: " + groupname + " Msg: " + newMsg
+              "New Message received in Group:",
+              groupname,
+              "Msg:",
+              newMsg
             );
 
-            group.messages.push(newMsg);
-
-            connections.forEach((value: SocketStream, key: string) => {
-              if (key.startsWith(groupname)) {
-                value.socket.send(
-                  JSON.stringify({
-                    operation: "NEW_MESSAGE",
-                    payload: { user, msg: newMsg },
-                  })
-                );
-              }
+            group.conns.forEach((conn) => {
+              conn.socket.send(
+                JSON.stringify({
+                  operation: "NEW_MESSAGE",
+                  payload: { user, msg: newMsg },
+                })
+              );
             });
           }
           break;
       }
     });
 
-    connection.socket.on("close", () => {
-      console.log("Client has been disconnected");
-      for (const [key, value] of connections) {
-        if (value === connection) {
-          const valueSplit = key.split("_");
-          const groupname = valueSplit[0];
-          const userOfCon = valueSplit[1];
-
-          console.log(
-            "User: " +
-              userOfCon +
-              " in Group: " +
-              groupname +
-              " has been disconnected"
-          );
-
-          connections.delete(key);
-
-          dbClient
-            .get(groupname)
-            .json()
-            .then((group) => {
-              if (group) {
-                group["users"] = group["users"].filter(
-                  (user: string) => user !== userOfCon
-                );
-                dbClient.put(groupname).value(JSON.stringify(group)).exec();
-              }
-            });
-
-          break;
-        }
-      }
-    });
+    conn.socket.on("close", () => disconnect(conn));
   });
 });
 
 const { BACKEND_HOST, BACKEND_PORT } = process.env;
 
-server.listen({ host: BACKEND_HOST, port: Number(BACKEND_PORT) }, (err) => {
+console.log(process.argv[2]);
+const portInput = process.argv[2];
+const port = portInput ? Number(portInput) : Number(BACKEND_PORT);
+
+server.listen({ host: BACKEND_HOST, port }, (err) => {
   if (err) {
     server.log.error(err);
     process.exit(1);
