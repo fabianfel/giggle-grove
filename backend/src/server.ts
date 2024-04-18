131f19137a7a2725fd857cee46aedd0630da80db
fabianfel <70578359+fabianfel@users.noreply.github.com>
2024-04-18 09:42:22 +0200

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,12 +7,13 @@ const { BACKEND_HOST, BACKEND_PORT } = process.env;
 const portInput = process.argv[2];
 const port = portInput ? Number(portInput) : Number(BACKEND_PORT);
 
-const app = build({
+const { app, disconnectServers } = build({
   logger: true,
 });
 
 app.listen({ host: BACKEND_HOST, port }, (err) => {
   if (err) {
+    disconnectServers();
     app.log.error(err);
     process.exit(1);
   }
